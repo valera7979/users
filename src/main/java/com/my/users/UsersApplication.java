9b5ac21c13ccaf314775dcca7b561b9f6496ec58
Valerii Kalynovskyi <valerii.kalynovskyi@devexperts.com>
2022-05-21 21:38:05 +0300

    added comment

--- src/main/java/com/my/users/UsersApplication.java
+++ src/main/java/com/my/users/UsersApplication.java
@@ -2,7 +2,6 @@ package com.my.users;
 
 import com.my.users.service.MyService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,10 +14,11 @@ public class UsersApplication implements ApplicationRunner {
 		SpringApplication.run(UsersApplication.class, args);
 	}
 
-	@Autowired
-	private MyService myService;
+	private final MyService myService;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		// See https://habr.com/ru/post/347752/
 		myService.method1();
 	}
 }
